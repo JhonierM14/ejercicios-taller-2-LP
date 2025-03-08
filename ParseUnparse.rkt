b7cc9ff1c677dfba36813f0566e26b210b441a67
JhonierM14 <144240957+JhonierM14@users.noreply.github.com>
2025-03-07 20:55:20 -0500

    Update ParseUnparse.rkt

--- ParseUnparse.rkt
+++ ParseUnparse.rkt
@@ -1,5 +1,18 @@
+;;-----------------------------------------
+;; Jhonier Mendez Bravo 202372226
+;; David Santiago Guerrero Delgado 202324594
+;; Juan Pablo Robayo Maestre 202156743
+;;-----------------------------------------
+
+#lang eopl
+
 #|
 para la representacion basada en listas, construya una funcion PARSEBNF
 donde dada una lista con la representacion concreta de un circuito,
 construya el arbol de sintaxis abstracta basado en datatypes.
+
+para la representacion basada en datatypes, construya 
+una funcion UNPARSEBNF donde dado un arbol de 
+sintaxis abstracta de un circuito, entregue la 
+representacion concreta basada en listas.
 |#
