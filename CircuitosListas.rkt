8ee653ae8bf9e5b2c376a5c587fafba77f2fabad
JhonierM14 <144240957+JhonierM14@users.noreply.github.com>
2025-03-07 20:44:11 -0500

    Update CircuitosListas.rkt

--- CircuitosListas.rkt
+++ CircuitosListas.rkt
@@ -5,3 +5,16 @@
 ;;-----------------------------------------
 
 #lang eopl
+
+#|
+(20 Pts) En un archivo llamado CircuitosListas.rkt, proponga
+una implementacion de la gram´tica basada en listas: Esta 
+implementacion deberia contener los respectivos constructores (circuit,
+gate list, gate, type, input list). gate id y gate ref son simbolos y
+no requeren un constructor. La implementacion tambien debe contener 
+extractores (circuit− > gate list, gate list− > first, gate list− >
+rest, gate− > gate id, gate− > type, gate− > input list, input list− >
+first, input list− > rest). Incluya ejemplos donde se evidencie
+su utilizacion y la creacion de por lo menos los 4 ejemplos de este
+taller.
+|#
