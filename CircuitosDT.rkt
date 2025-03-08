c2a5b9da849d383cd2f2ce45adb60ebcf672731c
JhonierM14 <144240957+JhonierM14@users.noreply.github.com>
2025-03-07 20:47:25 -0500

    Create CircuitosDT.rkt

---
+++ CircuitosDT.rkt
@@ -0,0 +1,18 @@
+;;-----------------------------------------
+;; Jhonier Mendez Bravo 202372226
+;; David Santiago Guerrero Delgado 202324594
+;; Juan Pablo Robayo Maestre 202156743
+;;-----------------------------------------
+
+#lang eopl
+
+#|
+(20 Pts) proponga una implementacion basada en datatypes 
+(hay varias formas como puede representar la gramatica, 
+en especial dependiendo del manejo que le de a las listas 
+gate list e input list). Incluya ejemplos donde
+se evidencie su utilizacion y la creacion de por lo menos 
+los 4 ejemplos de este taller. Revise los ejemplos de 
+sintaxis abstracta de la siguiente seccion que le podran 
+servir como guia.
+|#
