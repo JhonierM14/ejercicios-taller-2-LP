f3ba76f4a5a09d1dcc290538b210637e3e11cda5
JhonierM14 <144240957+JhonierM14@users.noreply.github.com>
2025-03-07 20:51:07 -0500

    Create ParseUnparse.rkt

---
+++ ParseUnparse.rkt
@@ -0,0 +1,5 @@
+#|
+para la representacion basada en listas, construya una funcion PARSEBNF
+donde dada una lista con la representacion concreta de un circuito,
+construya el arbol de sintaxis abstracta basado en datatypes.
+|#
