f556524e83a523c486125389ef0610984abd0dac
JhonierM14 <144240957+JhonierM14@users.noreply.github.com>
2025-03-09 19:50:49 -0500

    Update ParseUnparse.rkt
    IDEA

--- ParseUnparse.rkt
+++ ParseUnparse.rkt
@@ -15,4 +15,36 @@ para la representacion basada en datatypes, construya
 una funcion UNPARSEBNF donde dado un arbol de 
 sintaxis abstracta de un circuito, entregue la 
 representacion concreta basada en listas.
-|#
+
+;;---------------------------------
+;;
+;;       FALTA DATATYPE
+;;
+;;---------------------------------
+
+(define isgate_list?
+  (lambda (L)
+    (if (null? L) #t (if (eqv? (caar L) 'gate) (isgate_list? (cdr L)) #f) )
+    ))
+
+(define isgate?
+  (lambda (L)
+    (if (eqv? (car L) 'gate) #t #f)
+    ))
+
+(define PARSEBNF
+  (lambda (circuit)
+    (cond
+      [(null? circuit) circuit]
+      [(symbol? circuit) (constuirSimbolo circuit)]
+      [(eqv? (car circuit) 'circuit) (construirCircuito PARSEBNF(cdr circuit))]
+      [(isgate_list? circuit) (construirGateList (car circuit) PARSEBNF(cdr circuit))]
+      [(isgate? circuit) (construirGate PARSEBNF(circuit))]
+      [else (eopl:error "Invalid concrete syntax" circuit)]
+      )))
+
+;;Pruebas
+(PARSEBNF
+ '(circuit
+       (gate-list
+           (gate G1 (type not) (input-list #t)))))
